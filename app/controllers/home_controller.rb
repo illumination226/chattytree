619b10c46376501cbf820c32ff516fe810b9ce08
illumination226 <illuminationdevelopment@gmail.com>
2011-04-15 23:06:38 -0700

    added example xml pull

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,20 @@
 class HomeController < ApplicationController
   def index
+	  xml = File.read('http://static.railstips.org/images/articles/2008/8/9/timeline.xml')
+	  puts Benchmark.measure {
+	  doc, statuses = REXML::Document.new(xml), []
+	  doc.elements.each('statuses/status') do |s|
+	    h = {:user => {}}
+	    %w[created_at id text source truncated in_reply_to_status_id in_reply_to_user_id favorited].each do |a|
+	      h[a.intern] = s.elements[a].text
+	    end
+	    %w[id name screen_name location description profile_image_url url protected followers_count].each do |a|
+	      h[:user][a.intern] = s.elements['user'].elements[a].text
+	    end
+	    statuses << h
+	  end
+	  # pp statuses
+	  }
   end
 
 end
