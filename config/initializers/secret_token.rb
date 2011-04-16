0706db5637679162a031b0c81885a706047991a3
illumination226 <illuminationdevelopment@gmail.com>
2011-04-16 05:59:00 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chattytree::Application.config.secret_token = '3efa32b3558582550d786c34fe5c03d35a7b20c1ed120da6990e60af8706714db3539ec72dba51a6ec061d9d5f7ce9a639b55c062f146f445fda9eb2b39e657b'
