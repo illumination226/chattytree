35eac7dc07f4b6c64166def3d0fad60f44fb3617
illumination226 <illuminationdevelopment@gmail.com>
2011-04-16 22:55:34 -0700

    added rss feed reader function

--- app/controllers/groupon_controller.rb
+++ app/controllers/groupon_controller.rb
@@ -1,2 +1,22 @@
 class GrouponController < ApplicationController
+  def index
+  	@output = parseFeed("http://feeds.feedburner.com/groupon")
+  end
+  
+    def parseFeed (url)
+      require 'rss/2.0'
+	  require 'open-uri'
+      feed_url = url
+	  output = "" 
+	  open(feed_url) do |http|
+	    response = http.read
+	    result = RSS::Parser.parse(response, false)
+	     output += "<span class=\"feedTitle\">#{result.channel.title}</span><br /><ul>" 
+	    result.items.each_with_index do |item, i|
+	     	 output += "<li><a href=\"#{item.link}\">#{item.title}</a><p>#{item.description}</p></li>"
+	    end 
+	    output += "</ul>" 
+	  end   
+      return output
+   end   
 end
